cdab46b94b50ad4377fbb0cb49b0c0c5e3674efe
xAdvitya <advityasharma21@gmail.com>
2022-10-01 23:37:51 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Button } from '@material-ui/core';
+
+
+function App() {
+  return <div className="App">
+    <Button color="primary">Movie Buff</Button>
+  </div>;
+}
+
+export default App;

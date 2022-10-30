323de00e1d25d00bf27a001c73311dafd11841ba
xAdvitya <advityasharma21@gmail.com>
2022-10-30 23:18:11 +0300

    removing unnecessary imports

--- src/components/UI/MovieCard.js
+++ src/components/UI/MovieCard.js
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, Grid } from '@mui/material';
-import { MovieSharp } from '@material-ui/icons';
 
 const MovieCard = (props) => {
   const loadMoreHandler = () => {
--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import DropDown from './DropDown';
 import { Link, Route } from 'react-router-dom';
-import MovieModal from './MovieModal';
+
 
 const NavBar = (props) => {
   return (

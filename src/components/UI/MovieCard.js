291d39212c1f7a8421668b66d9a7d9927e72d9d9
xAdvitya <advityasharma21@gmail.com>
2022-11-02 15:19:55 +0530

    fixing grid spacing

--- src/components/UI/MovieCard.js
+++ src/components/UI/MovieCard.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, Grid } from '@mui/material';
+import { Button, CardActionArea, Container, Grid } from '@mui/material';
 
 const MovieCard = (props) => {
   const loadMoreHandler = () => {
@@ -22,40 +22,47 @@ const MovieCard = (props) => {
 
   return (
     <>
-      <Grid container direction={'row'}  justifyContent="space-between" spacing={6}>
-        {props.movies.map((movie) => (
-          <Grid item xs={12} sm={4}>
-            <Card sx={{ maxWidth: 345, backgroundColor: '#ece3e3' }}>
-              <CardActionArea>
-                {movie.poster_path && (
-                  <CardMedia
-                    component="img"
-                    image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                )}
+      <Container>
+        <Grid
+          container
+          direction={'row'}
+          justifyContent="space-between"
+          spacing={6}
+        >
+          {props.movies.map((movie) => (
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ maxWidth: 345, backgroundColor: '#ece3e3' }}>
+                <CardActionArea>
+                  {movie.poster_path && (
+                    <CardMedia
+                      component="img"
+                      image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  )}
 
-                {!movie.poster_path && (
-                  <CardMedia
-                    component="img"
-                    image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
-                    alt={movie.title}
-                  />
-                )}
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {movie.title}
-                  </Typography>
+                  {!movie.poster_path && (
+                    <CardMedia
+                      component="img"
+                      image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+                      alt={movie.title}
+                    />
+                  )}
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {movie.title}
+                    </Typography>
 
-                  <Typography variant="body2" color="text.secondary">
-                    {movie.overview}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                    <Typography variant="body2" color="text.secondary">
+                      {movie.overview}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
 
       <Grid container direction={'row'} justifyContent="center" spacing={6}>
         <Grid item xs="auto">
